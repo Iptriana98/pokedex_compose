bec66afa04c20794f4ccfc9d8d0a74f1f894caba
Iptriana98 <78894683+Iptriana98@users.noreply.github.com>
2022-12-22 15:26:13 -0500

    - setup pokedex entry and pokedex row

--- app/src/main/java/cu/iptriana/pokedexcompose/ui/composables/pokemon_list/PokedexEntry.kt
+++ app/src/main/java/cu/iptriana/pokedexcompose/ui/composables/pokemon_list/PokedexEntry.kt
@@ -1,11 +1,33 @@
 package cu.iptriana.pokedexcompose.ui.composables.pokemon_list
 
-import androidx.compose.runtime.Composable
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.CircularProgressIndicator
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Alignment.Companion.CenterHorizontally
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.scale
+import androidx.compose.ui.draw.shadow
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.toArgb
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavController
+import coil.compose.AsyncImage
+import coil.request.ImageRequest
 import cu.iptriana.pokedexcompose.data.model.PokedexListEntry
+import cu.iptriana.pokedexcompose.ui.theme.Roboto
 import cu.iptriana.pokedexcompose.ui.viewmodels.PokemonListViewModel
+import cu.iptriana.pokedexcompose.util.Constants.POKEMON_DETAIL_SCREEN
 
 @Composable
 internal fun PokedexEntry(
@@ -13,6 +35,57 @@ internal fun PokedexEntry(
     navController: NavController,
     modifier: Modifier = Modifier,
     viewModel: PokemonListViewModel = hiltViewModel()
-){
+) {
+    var defaultDominantColor = MaterialTheme.colors.surface
+    var dominantColor by remember {
+        mutableStateOf(defaultDominantColor)
+    }
+    Box(
+        contentAlignment = Alignment.Center,
+        modifier = modifier
+            .shadow(5.dp, RoundedCornerShape(10.dp))
+            .clip(RoundedCornerShape(10.dp))
+            .aspectRatio(1f)
+            .background(
+                Brush.verticalGradient(
+                    listOf(dominantColor, defaultDominantColor)
+                )
+            )
+            .clickable {
+                navController.navigate(
+                    "$POKEMON_DETAIL_SCREEN/${dominantColor.toArgb()}/${entry.pokemonName}"
+                )
+            }
+    ) {
+        Column {
+            CircularProgressIndicator(
+                color = MaterialTheme.colors.primary,
+                modifier = Modifier.scale(0.5f)
+            )
 
+            AsyncImage(
+                model = ImageRequest.Builder(LocalContext.current)
+                    .data(entry.imageUrl)
+                    .crossfade(true)
+                    .target {
+                        viewModel.calcDominantColor(it) { color ->
+                            dominantColor = color
+                        }
+                    }.build(),
+                contentDescription = entry.pokemonName,
+                modifier = Modifier
+                    .size(120.dp)
+                    .align(CenterHorizontally)
+            )
+
+            Text(
+                text = entry.pokemonName,
+                fontFamily = Roboto,
+                fontSize = 20.sp,
+                textAlign = TextAlign.Center,
+                modifier = Modifier.fillMaxWidth()
+            )
+        }
+
+    }
 }
---
+++ app/src/main/java/cu/iptriana/pokedexcompose/ui/composables/pokemon_list/PokedexRow.kt
@@ -0,0 +1,36 @@
+package cu.iptriana.pokedexcompose.ui.composables.pokemon_list
+
+import androidx.compose.foundation.layout.*
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import androidx.navigation.NavController
+import cu.iptriana.pokedexcompose.data.model.PokedexListEntry
+
+@Composable
+internal fun PokedexRow(
+    rowIndex: Int,
+    entries: List<PokedexListEntry>,
+    navController: NavController
+) {
+    Column {
+        Row {
+            PokedexEntry(
+                entry = entries[rowIndex * 2],
+                navController = navController,
+                modifier = Modifier.weight(1f)
+            )
+            Spacer(modifier = Modifier.width(16.dp))
+            if (entries.size >= rowIndex * 2 + 2){
+                PokedexEntry(
+                    entry = entries[rowIndex * 2 + 1],
+                    navController = navController,
+                    modifier = Modifier.weight(1f)
+                )
+            } else {
+                Spacer(modifier = Modifier.weight(1f))
+            }
+        }
+        Spacer(modifier = Modifier.height(16.dp))
+    }
+}
